376734c63b2351a3f50bf9f7ea148bb3e3eeb7b7
acquiesce22 <113950384+acquiesce22@users.noreply.github.com>
2022-09-20 09:58:17 -0400

    diet coke

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ctypes import sizeof
 from vendingMachineFuncts import vendingMachine
 
 while(1):
@@ -6,13 +7,29 @@ while(1):
     inp = inp.lower().split()
 
 
+
+
     match inp[0]:
         case "add":
-            vendingMachine.addItem(inp[2], int(inp[3]), float(inp[4]))
+            if (len(inp) >= 5):
+                inp[2] = inp[2] + ' ' + inp[3]
+                inp[3] = inp [4]
+                inp[4] = inp[5]
+                vendingMachine.addItem(inp[2], int(inp[3]), float(inp[4]))
+            else:
+                vendingMachine.addItem(inp[2], int(inp[3]), float(inp[4]))
         case "exit":
             vendingMachine.exit()
         case "buy":
-            vendingMachine.buyItem(inp[2], int(inp[3]), int(inp[4]), int(inp[5]), int(inp[6]), int(inp[7]))
+            if (len(inp) > 7):
+                inp[2] = inp[2] + ' ' + inp[3]
+                inp[3] = inp[4]
+                inp[4] = inp[5]
+                inp[5] = inp[6]
+                inp[6] = inp[7]
+                vendingMachine.buyItem(inp[2], int(inp[3]), int(inp[4]), int(inp[5]), int(inp[6]), int(inp[7]))
+            else:
+                vendingMachine.buyItem(inp[2], int(inp[3]), int(inp[4]), int(inp[5]), int(inp[6]), int(inp[7]))
         case "help":
             vendingMachine.help()
         case "inventory":
